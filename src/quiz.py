1e9ad77a40ede9002c645bef5b4502b2678e74fb
molanp <104612722+molanp@users.noreply.github.com>
2025-08-19 17:54:16 +0800

    docs(README): 添加 Web API 说明和数据结构示例
    - 在 README.md 中添加了 Web API 接口说明
    - 提供了获取历史题目数据和当前题目数据的 API 路径
    - 添加了数据结构示例，展示了题目数据的 JSON 格式

--- src/quiz.py
+++ src/quiz.py
@@ -2,16 +2,30 @@ from playwright.sync_api import sync_playwright
 import re
 import time
 
+
 def fetch_quiz_results():
     with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(headless=True)
-        context = browser.new_context(locale='zh-CN')
+        browser = playwright.chromium.launch(headless=False)
+        context = browser.new_context(locale="zh-CN")
         page = context.new_page()
-        page.goto("https://www.bing.com/search?q=Bing%20Homepage%20quiz&form=ML2BF1&OCID=ML2BF1&mkt=zh-CN")
+        page.goto(
+            "https://www.bing.com/search?q=Bing%20Homepage%20quiz&form=ML2BF1&OCID=ML2BF1&mkt=zh-CN"
+        )
+        def handle_page(new_page):
+            page.goto(new_page.url)
+            new_page.close()
+            
+        context.on("page", handle_page)
+        
         print("Current URL: ", page.url)
         answers = []
         for i in range(3):
             time.sleep(1)
+            choices = []
+            choices.extend(
+                (page.locator(f"#ChoiceText_{i}_{c} > div").first.text_content() or "")
+                for c in range(3)
+            )
             page.locator(f"#questionOptionChoice{i}1").click()
             question = page.locator(".wk_headingPadding").text_content() or ""
             if page.locator(".wk_correctAns").count() == 1:
@@ -26,12 +40,21 @@ def fetch_quiz_results():
                 rep = re.search(r"(\d+%)", rate)
                 assert rep
                 rate = rep[1]
-            print(f"{question.strip()} {answer.strip()} {rate.strip()}")
-            answers.append({
-                "question": question.strip(),
-                "answer": answer.strip(),
-                "rate": rate.strip()
-            })
+            
+            print(f"-----------------Topic {i}-----------------")
+            print("Question:",question.strip())
+            print("Choices:", choices)
+            print("Answer:",answer.strip())
+            print("Accuracy:",rate)
+            answers.append(
+                {
+                    "question": question.strip(),
+                    "answer": answer.strip(),
+                    "choices": choices,
+                    "rate": rate.strip(),
+                }
+            )
+            time.sleep(1)
             if i < 2:
                 page.locator(f"#nextQuestionbtn{i}").click()
         context.close()
